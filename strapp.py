26bee06490a0018d6b61823848a61c8cd249c29a
GauravBiraris <136731857+GauravBiraris@users.noreply.github.com>
2024-02-23 15:52:24 +0530

    Update strapp.py

--- strapp.py
+++ strapp.py
@@ -12,21 +12,12 @@ indexer = Indexer()
 catalog.add_item(Item("Dummy 1", {"Mega haul"}))
 catalog.add_item(Item("Dummy 2", {"Intense orange yellow pigment"}))
 
-# writer = BufferedWriter(indexer.index, period=120, limit=20)
-
 st.title('My Catalog App')
 
 # Index catalog
 st.write('Indexing catalog...')
-writer = BufferedWriter(indexer.index)
-try:
-  writer.lock()
-  indexer.index_catalog(catalog)
-  st.write('Done!')
-  writer.commit()
-finally:
-  writer.unlock()
-
+# indexer.index_catalog(catalog)
+st.write('Done!')
 
 # Add item
 st.header('Add New Item')
@@ -34,15 +25,9 @@ name = st.text_input('Name')
 desc = st.text_area('Description')
 if st.button('Add'):
   item = Item(name, desc) 
-  writer = BufferedWriter(indexer.index)
-  try:
-    writer.lock()
-    indexer.index_item(item)
-    catalog.add_item(item)
-    st.success('Item added!')
-    writer.commit()
-  finally:
-    writer.unlock()
+  indexer.index_item(item)
+  catalog.add_item(item)
+  st.success('Item added!')
 
 
 # Update item
@@ -52,43 +37,25 @@ name = st.text_input('New name', value=catalog.get_item(selected_id).name)
 desc = st.text_area('New description', value=catalog.get_item(selected_id).desc)
 if st.button('Update'):
   item = catalog.get_item(selected_id)
-  writer = BufferedWriter(indexer.index)
   if item:
-    try:
-      name = st.text_input("New name", value=item.name)
-      writer.lock()
-      item.name = name
-      item.desc = desc
-      indexer.update_item(item)
-      writer.commit()
-      st.success('Item updated!')
-    finally:
-      writer.unlock()
+    name = st.text_input("New name", value=item.name)
+    item.name = name
+    item.desc = desc
+    indexer.update_item(item)
+    st.success('Item updated!')
   else:
     st.warning("Item not found")
 
 # Delete item
 selected_id = st.selectbox('Select item to delete', options=items.keys())
 if st.button('Delete'):
-  writer = BufferedWriter(indexer.index)
-  try:
-    writer.lock() 
-    indexer.delete_item(selected_id)
-    catalog.delete_item(selected_id)
-    writer.commit()
-    st.warning('Item deleted!')
-  finally:
-    writer.unlock()
+  indexer.delete_item(selected_id)
+  catalog.delete_item(selected_id)
+  st.warning('Item deleted!')
 
 # Search 
 query = st.text_input('Search')  
 if query:
-  writer = BufferedWriter(indexer.index)
-  try:
-    writer.lock() 
-    writer.commit()
-    results = indexer.search(query)
-    for rank, item in enumerate(results):
-      st.write(f'{rank+1}. {item.name} ({item.id})')
-  finally:
-    writer.unlock()
+  results = indexer.search(query)
+  for rank, item in enumerate(results):
+    st.write(f'{rank+1}. {item.name} ({item.id})')
